f66f6fc2a0a3b5bb3f1294f740b809ed835174b2
Glitch (hello-express) <none>
2021-08-01 19:31:31 +0000

    👘💃 Checkpoint ./config.json:11536694/3 ./commands/volume.js:11536694/29

--- commands/volume.js
+++ commands/volume.js
@@ -12,11 +12,11 @@ module.exports = {
 
     if (!args[0]) return message.channel.send(`<a:azul_cdRDM:871231604465479710> **${message.author.username}**, O volume atual é: __${queue.volume}%__`).catch(console.error);
     if (isNaN(args[0])) return message.channel.send(`<a:v_alert:853079364651450418> **${message.author.username}**, Por favor, use um número para definir o volume.`).catch(console.error);
-    if (parseInt(args[0]) > 100 || parseInt(args[0]) < 0)
+    if (parseInt(args[0]) > 1000 || parseInt(args[0]) < 0)
       return message.channel.send(`<:errado_rosa:828670964903575562> Parece que você usou o comando de forma errada, **${message.author.username}\nUso correto:** _${message.client.prefix}volume <1-100>_`).catch(console.error);
 
     queue.volume = args[0];
-    queue.connection.dispatcher.setVolumeLogarithmic(args[0] / 2000);
+    queue.connection.dispatcher.setVolumeLogarithmic(args[0] / 1000);
 
     return queue.textChannel.send(`<a:azul_cdRDM:871231604465479710> **${message.author.username}**, alterou o volume da musica para: __${args[0]}%__`).catch(console.error);
   }
